d4328a42328de4b63670d86e7ba75b35a05199df
krystianlab98 <krystianlab98@gmail.com>
2021-06-12 14:29:11 +0200

    Added docker configuration

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require 'Router.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+Router::get('login', 'DefaultController');
+
+Router::run($path);
