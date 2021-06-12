de5c59d6c10a444d00a0f20c0458063f5af7265e
krystianlab98 <krystianlab98@gmail.com>
2021-06-12 14:30:13 +0200

    Added main and login controllers

---
+++ src/controllers/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'AppController.php';
+
+class DefaultController extends AppController {
+
+    public function index()
+    {
+        $this->render('index');
+    }
+
+    public function login()
+    {
+        $this->render('login');
+    }
+}
